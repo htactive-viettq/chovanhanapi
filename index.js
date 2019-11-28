f6aa704494dcf632dac6c217aff7c5c36b16453b
htactive-viettq <viettq@htactive.com>
2019-11-28 15:07:13 +0700

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
+const cors = require("cors")
 const router = require('./src/router')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors())
 
 dotenv.config()
 
